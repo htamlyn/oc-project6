2be41852d5a9fe4b60867a9da298bc8a6739258e
htamlyn <helentamlyn@gmail.com>
2021-06-15 09:07:09 +0100

    Ensure all routes authorised

--- routes/sauce.js
+++ routes/sauce.js
@@ -7,7 +7,7 @@ const multer = require('../middleware/multer-config');
 const sauceCtrl = require('../controllers/sauce');
 
 router.get('/', auth, sauceCtrl.getAllSauces);
-router.get('/:id', sauceCtrl.getOneSauce);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.post('/', auth, multer, sauceCtrl.createSauce);
 router.post('/:id/like', auth, sauceCtrl.likeSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
